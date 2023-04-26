3db9291d56b23dcf1792fc8c3c40120ed4ca3fab
YaRo <roman.yatsenko@hneu.net>
2023-04-26 16:59:31 +0300

    Add order models to DB

--- orders/models.py
+++ orders/models.py
@@ -29,7 +29,7 @@ class Order(models.Model):
         return sum(item.get_cost() for item in self.items.all())
 
 
-class OrderItem(modelf.Model):
+class OrderItem(models.Model):
     """Order Item Details"""
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='order_items')
